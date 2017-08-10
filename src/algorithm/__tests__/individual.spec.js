4b1ff3afec6270d6d90ea72a6563d6d3c26b5c53
Patrick Kim <ptrkkim@gmail.com>
2017-08-10 16:26:26 -0400

    tests: individuals can have polygon count, vertex count, and dna passed into constructor

--- src/algorithm/__tests__/individual.spec.js
+++ src/algorithm/__tests__/individual.spec.js
@@ -2,16 +2,20 @@ import { Gene } from '../gene';
 import { Individual } from '../individual';
 
 describe('Individuals', () => {
-  const generateSpy = jest.spyOn(Individual.prototype, 'generate');
+  const generateIndividualSpy = jest.spyOn(Individual.prototype, 'generate');
   const geneRgbaSpy = jest.spyOn(Gene.prototype, 'generateRgba');
   const genePointsSpy = jest.spyOn(Gene.prototype, 'generatePoints');
+  let testImage;
+  const numPolygons = 50;
 
   describe('can self-generate', () => {
-    const numPolygons = 50;
-    const testImage = new Individual(numPolygons);
+    beforeEach(() => {
+      jest.clearAllMocks();
+      testImage = new Individual(numPolygons);
+    });
 
     test('using prototypical generate method to populate own dna', () => {
-      expect(generateSpy).toHaveBeenCalledTimes(1);
+      expect(generateIndividualSpy).toHaveBeenCalledTimes(1);
     });
 
     test('with a property defining the number of polygons in the image', () => {
@@ -35,7 +39,35 @@ describe('Individuals', () => {
   });
 
   describe('initialize when passed a dna value', () => {
+    const numVertices = 5;
+    beforeEach(() => {
+      const dna50 = Array(numPolygons).fill('').map(() => new Gene(numVertices));
+      jest.clearAllMocks();
+      testImage = new Individual(numPolygons, numVertices, dna50);
+    });
 
+    test('without calling the prototypical generate method', () => {
+      expect(generateIndividualSpy).not.toHaveBeenCalled();
+      expect(geneRgbaSpy).not.toHaveBeenCalled();
+      expect(genePointsSpy).not.toHaveBeenCalled();
+    });
+
+    test('using the correct n-polygon', () => {
+      for (let i = 3; i < 10; i++) {
+        const nPolygonIndividual = new Individual(10, i);
+        expect(nPolygonIndividual).toHaveProperty('verticesPerPolygon', i);
+      }
+    });
+
+    test('use polygons with as many vertices as specified', () => {
+      testImage.dna.forEach(gene => expect(gene.numVertices).toBe(numVertices));
+      testImage.dna.forEach(gene => expect(gene.points).toHaveLength(numVertices));
+    });
+
+    test('with as many genes as there are in the dna passed to it', () => {
+      expect(testImage).toHaveProperty('numPolygons', numPolygons);
+      expect(testImage.dna).toHaveLength(numPolygons);
+    });
   });
 
   describe('mutate', () => {
