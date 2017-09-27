27ea115804ed4be324a9d60929239c1e8dd5363f
Patrick Kim <ptrkkim@gmail.com>
2017-09-27 19:16:12 -0400

    make navbar somewhat responsive

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { navbar, inactive, active, header, title, thin, closer } from './styles/navbar.css';
+import { navbar, inactive, active, header, titleBox, genetic, gallery, linkBox } from './styles/navbar.css';
 
 export default () => (
   <nav className={navbar}>
-    <NavLink to="/" exact className={inactive} activeClassName={active}>
-      <div className={title}>
-        <h2 className={thin}>GENETIC</h2>
-        <h2 className={closer}>GALLERY</h2>
-      </div>
-    </NavLink>
-    <NavLink to="/create" className={inactive} activeClassName={active}>
-      <div className={header}>
-        <h2>CREATE</h2>
-      </div>
-    </NavLink>
-    <NavLink to="/learn" className={inactive} activeClassName={active}>
-      <div className={header}>
-        <h2>LEARN</h2>
-      </div>
-    </NavLink>
+    <div className={linkBox}>
+      <NavLink to="/" exact className={inactive} activeClassName={active}>
+        <div className={titleBox}>
+          <h2 className={genetic}>GENETIC</h2>
+          <h2 className={gallery}>GALLERY</h2>
+        </div>
+      </NavLink>
+      <NavLink to="/create" className={inactive} activeClassName={active}>
+        <div className={header}>
+          <h2>CREATE</h2>
+        </div>
+      </NavLink>
+      <NavLink to="/learn" className={inactive} activeClassName={active}>
+        <div className={header}>
+          <h2>LEARN</h2>
+        </div>
+      </NavLink>
+    </div>
   </nav>
 );
